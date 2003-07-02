553a3e4821ee4147439685af0016bd8078034e17
Justin Karneges <justin@affinix.com>
2003-07-02 01:21:23 +0000

    template madness
    svn path=/trunk/kdesupport/qca/; revision=235005

--- src/qca.h
+++ src/qca.h
@@ -4,24 +4,68 @@
 #include<qstring.h>
 #include<qcstring.h>
 
+#include<stdio.h>
+
 namespace QCA
 {
 	enum {
 		CAP_SHA1      = 0x0001,
-		CAP_MD5       = 0x0002,
-		CAP_TripleDES = 0x0004,
-		CAP_AES128    = 0x0008,
-		CAP_AES256    = 0x0010,
-		CAP_X509      = 0x0020,
-		CAP_TLS       = 0x0040,
-		CAP_SASL      = 0x0080,
-		CAP_PGP       = 0x0100,
+		CAP_SHA256    = 0x0002,
+		CAP_MD5       = 0x0004,
+		CAP_TripleDES = 0x0008,
+		CAP_AES128    = 0x0010,
+		CAP_AES256    = 0x0020,
+		CAP_X509      = 0x0040,
+		CAP_TLS       = 0x0080,
+		CAP_SASL      = 0x0100,
+		CAP_PGP       = 0x0200,
 	};
 
 	void init();
 	bool isSupported(int capabilities);
 
-	class SHA1
+	QString arrayToHex(const QByteArray &);
+
+	class Hash
+	{
+	public:
+		Hash() {}
+		virtual ~Hash() {}
+
+		virtual void clear()=0;
+		virtual void update(const QByteArray &)=0;
+		virtual QByteArray final()=0;
+	};
+
+	template <class T> class HashStatic
+	{
+	public:
+		static QByteArray hash(const QByteArray &a)
+		{
+			T obj;
+			obj.update(a);
+			return obj.final();
+		}
+
+		static QByteArray hash(const QCString &cs)
+		{
+			QByteArray a(cs.length());
+			memcpy(a.data(), cs.data(), a.size());
+			return hash(a);
+		}
+
+		static QString hashToString(const QByteArray &a)
+		{
+			return arrayToHex(hash(a));
+		}
+
+		static QString hashToString(const QCString &cs)
+		{
+			return arrayToHex(hash(a));
+		}
+	};
+
+	class SHA1 : public Hash, public HashStatic<SHA1>
 	{
 	public:
 		SHA1();
@@ -30,14 +74,9 @@ namespace QCA
 		void clear();
 		void update(const QByteArray &);
 		QByteArray final();
+	};
 
-		static QByteArray hash(const QByteArray &);
-		static QByteArray hash(const QCString &);
-		static QString hashToString(const QByteArray &);
-		static QString hashToString(const QCString &);
-	}
-
-	class MD5
+	class MD5 : public Hash, public HashStatic<MD5>
 	{
 	public:
 		MD5();
@@ -46,12 +85,7 @@ namespace QCA
 		void clear();
 		void update(const QByteArray &);
 		QByteArray final();
-
-		static QByteArray hash(const QByteArray &);
-		static QByteArray hash(const QCString &);
-		static QString hashToString(const QByteArray &);
-		static QString hashToString(const QCString &);
-	}
+	};
 };
 
 #endif
