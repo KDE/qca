7f655159181e94548a460e0cc652ada625ef37c4
Justin Karneges <justin@affinix.com>
2003-09-16 02:57:37 +0000

    add license info, conform to slight change in qca
    svn path=/trunk/kdesupport/qca/; revision=251430

--- plugins/qca-tls/qca-tls.cpp
+++ plugins/qca-tls/qca-tls.cpp
@@ -1,3 +1,23 @@
+/*
+ * qca-tls.cpp - TLS plugin for QCA
+ * Copyright (C) 2003  Justin Karneges
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 #include"qca-tls.h"
 
 #include<qregexp.h>
@@ -1339,9 +1359,8 @@ public:
 	QCAOpenSSL() {}
 	~QCAOpenSSL() {}
 
-	QString name() const
+	void init()
 	{
-		return "openssl";
 	}
 
 	int qcaVersion() const
--- plugins/qca-tls/qca-tls.h
+++ plugins/qca-tls/qca-tls.h
@@ -1,12 +1,32 @@
-#ifndef QCAOPENSSL_H
-#define QCAOPENSSL_H
+/*
+ * qca-tls.h - TLS plugin for QCA
+ * Copyright (C) 2003  Justin Karneges
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef QCA_TLS_H
+#define QCA_TLS_H
 
 #include"qcaprovider.h"
 
 #ifdef QCA_PLUGIN
 QCA_EXPORT QCAProvider *createProvider();
 #else
-QCAProvider *createProviderOpenSSL();
+QCAProvider *createProviderTLS();
 #endif
 
 #endif
