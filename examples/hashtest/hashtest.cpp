0405ff42d00e75f63dcbfca7b6f00dca9d9882d5
Brad Hards <bradh@frogmouth.net>
2004-11-02 09:08:29 +0000

    Convert to QCA2 approach.
    Justin - is this change correct? If so, I'll start updating the doco as
    well.
    
    CCMAIL: delta-affinix.com@lists.affinix.com
    
    svn path=/trunk/kdesupport/qca/; revision=359773

--- examples/hashtest/hashtest.cpp
+++ examples/hashtest/hashtest.cpp
@@ -3,20 +3,22 @@
 
 int main(int argc, char **argv)
 {
-	QCA::init();
+	QCA::Initializer init;
 	QCString cs = (argc >= 2) ? argv[1] : "hello";
 
-	if(!QCA::isSupported(QCA::CAP_SHA1))
+	if( !QCA::isSupported("sha1") )
 		printf("SHA1 not supported!\n");
 	else {
-		QString result = QCA::SHA1::hashToString(cs);
+		QCA::SHA1 sha1Hash;
+		QString result = sha1Hash.hashToString(cs);
 		printf("sha1(\"%s\") = [%s]\n", cs.data(), result.latin1());
 	}
 
-	if(!QCA::isSupported(QCA::CAP_MD5))
+	if( !QCA::isSupported("md5") )
 		printf("MD5 not supported!\n");
 	else {
-		QString result = QCA::MD5::hashToString(cs);
+		QCA::MD5 md5Hash;
+		QString result = md5Hash.hashToString(cs);
 		printf("md5(\"%s\") = [%s]\n", cs.data(), result.latin1());
 	}
 
