9ee806511d947490dd72f442c20c74adfe177b15
Brad Hards <bradh@frogmouth.net>
2004-12-31 01:10:40 +0000

    More API documentation, and preparation for generating the website purely from the source tree.
    svn path=/trunk/kdesupport/qca/; revision=374375

--- src/qca.h
+++ src/qca.h
@@ -65,48 +65,102 @@ class QCA_CipherContext;
 class QCA_CertContext;
 
 /**
- * \mainpage Qt Cryptographic Architecture
- *
- * This library provides an easy API for the following features:
- *   - Secure byte arrays (QSecureArray)
- *   - Arbitrary precision integers (QBigInteger)
- *   - Random number generation (QCA::Random)
- *   - SSL/TLS (TBC)
- *   - X509 certificate (Cert) (TBC)
- *   - Simple Authentication and Security Layer (SASL) (TBC)
- *   - RSA (TBC)
- *   - Hashing (QCA::Hash)
- *       - QCA::SHA0
- *       - QCA::SHA1
- *       - QCA::MD2
- *       - QCA::MD4
- *       - QCA::MD5
- *       - QCA::RIPEMD160
- *       - QCA::SHA256
- *       - QCA::SHA384
- *       - QCA::SHA512
- *   - Ciphers (QCA::Cipher)
- *       - BlowFish  (QCA::BlowFish)
- *       - Triple %DES (QCA::TripleDES)
- *       - %DES (QCA:DES)
- *       - AES (QCA::AES128, QCA::AES192, QCA::AES256)
- *   - Keyed Hash Message Authentication Code (QCA::HMAC)
- *       - QCA::SHA1
- *       - QCA::MD5
- *       - QCA::RIPEMD160
- *
- * Functionality is supplied via plugins.  This is useful for avoiding
- * dependence on a particular crypto library and makes upgrading easier,
- * as there is no need to recompile your application when adding or
- * upgrading a crypto plugin.  Also, by pushing crypto functionality into
- * plugins, your application is free of legal issues, such as export
- * regulation.
- *
- * And of course, you get a very simple crypto API for Qt, where you can
- * do things like:
- * \code
- * QString hash = QCA::SHA1().hashToString(blockOfData);
- * \endcode
+   \mainpage %Qt Cryptographic Architecture
+
+   Taking a hint from the similarly-named
+   <a href="http://java.sun.com/j2se/1.4/docs/guide/security/CryptoSpec.html">Java
+   Cryptography Architecture</a>, %QCA aims to provide a
+   straightforward and cross-platform cryptographic API, using Qt
+   datatypes and conventions.  %QCA separates the API from the
+   implementation, using plugins known as Providers.  The advantage
+   of this model is to allow applications to avoid linking to or
+   explicitly depending on any particular cryptographic library.
+   This allows one to easily change or upgrade Provider
+   implementations without even needing to recompile the
+   application!
+
+   %QCA should work everywhere %Qt does, including Windows/Unix/MacOSX.
+
+   \section features Features
+
+   This library provides an easy API for the following features:
+     - Secure byte arrays (QSecureArray)
+     - Arbitrary precision integers (QBigInteger)
+     - Random number generation (QCA::Random)
+     - SSL/TLS (TBC)
+     - X509 certificate (Cert) (TBC)
+     - Simple Authentication and Security Layer (SASL) (TBC)
+     - RSA (TBC)
+     - Hashing (QCA::Hash)
+         - QCA::SHA0
+         - QCA::SHA1
+         - QCA::MD2
+         - QCA::MD4
+         - QCA::MD5
+         - QCA::RIPEMD160
+         - QCA::SHA256
+         - QCA::SHA384
+         - QCA::SHA512
+     - Ciphers (QCA::Cipher)
+         - BlowFish  (QCA::BlowFish)
+         - Triple %DES (QCA::TripleDES)
+         - %DES (QCA::DES)
+         - AES (QCA::AES128, QCA::AES192, QCA::AES256)
+     - Keyed Hash Message Authentication Code (QCA::HMAC), using
+         - SHA1
+         - MD5
+         - RIPEMD160
+  
+   Functionality is supplied via plugins.  This is useful for avoiding
+   dependence on a particular crypto library and makes upgrading easier,
+   as there is no need to recompile your application when adding or
+   upgrading a crypto plugin.  Also, by pushing crypto functionality into
+   plugins, your application is free of legal issues, such as export
+   regulation.
+ 
+   And of course, you get a very simple crypto API for Qt, where you can
+   do things like:
+   \code
+   QString hash = QCA::SHA1().hashToString(blockOfData);
+   \endcode
+
+   \section using Using QCA
+
+   The application simply includes %qca.h and links to libqca,
+   which provides the 'wrapper API' and plugin loader.  Crypto
+   functionality is determined during runtime, and plugins are
+   loaded from the 'crypto' subfolder of the %Qt library paths. There
+   are <a href="examples.html">additional examples available</a>.
+
+   \section availability Availability
+
+   \subsection qca2code Current development
+
+   The latest version of the code is available from the KDE CVS
+   server (there is no formal release of the current version at this time). See
+   <a href="http://developer.kde.org/source/anoncvs.html">
+   http://developer.kde.org/source/anoncvs.html
+   </a> for general instructions. You do <i>not</i> need kdelibs or
+   arts modules for %QCA - just pull down kdesupport/qca. The plugins
+   are in the same tree. Naturally you will need %Qt properly set up
+   and configured in order to build and use %QCA.
+
+   The CVS code can also be browsed
+   <a href="http://webcvs.kde.org/cgi-bin/cvsweb.cgi/kdesupport/qca/">
+   via the web</a>
+
+   \subsection qca1code Previous versions
+   
+   A previous version of %QCA (sometimes referred to as QCA1) is still available.
+   You will need to get the main library 
+   (<a href="src/qca1/qca-1.0.tar.bz2">qca-1.0.tar.bz2</a>) and one or
+   more providers
+   (<a href="src/qca1/qca-tls-1.0.tar.bz2">qca-tls-1.0.tar.bz2</a> for
+   the OpenSSL based provider, or
+   <a href="src/qca1/qca-sasl-1.0.tar.bz2">qca-sasl-1.0.tar.bz2</a> for
+   the SASL based provider). Note that development of QCA1 has basically
+   stopped.
+
  */
 
 // Direct secure memory access.  For interfacing with C libraries if needed.
@@ -1221,14 +1275,44 @@ namespace QCA
 		virtual Context *createContext(const QString &type) = 0;
 	};
 
+	/**
+	 General superclass for buffered computation algorithms
+
+	 A buffered computation is characterised by having the
+	 algorithm take data in an incremental way, then having
+	 the results delivered at the end. Conceptually, the
+	 algorithm has some internal state that is modified
+	 when you call update() and returned when you call
+	 final().
+	*/
+	
 	class QCA_EXPORT BufferedComputation
 	{
 	public:
 		virtual ~BufferedComputation();
 
+		/**
+		   Reset the internal state
+		*/
 		virtual void clear() = 0;
+
+		/**
+		   Update the internal state with a byte array
+
+		   \param a the byte array of data that is to 
+		   be used to update the internal state.
+		*/
 		virtual void update(const QSecureArray &a) = 0;
+
+		/**
+		   Complete the algorithm and return the internal state
+		*/
 		virtual QSecureArray final() = 0;
+
+		/**
+		   Perform an "all in one" update, returning
+		   the result
+		*/
 		QSecureArray process(const QSecureArray &a);
 	};
 
