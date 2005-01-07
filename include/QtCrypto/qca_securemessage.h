8809df951ecbcd8d4cf5e189b67fced9cc073005
Justin Karneges <justin@affinix.com>
2005-01-07 08:53:39 +0000

    api for pgp/smime
    svn path=/trunk/kdesupport/qca/; revision=376035

--- include/QtCrypto/qca_securemessage.h
+++ include/QtCrypto/qca_securemessage.h
@@ -25,32 +25,122 @@
 #include <qobject.h>
 #include "qca_core.h"
 
+class QDateTime;
+
 namespace QCA
 {
+	class PrivateKey;
+	class Certificate;
+	class Store;
+	class SecureMessageSystem;
+
+	class SecureMessageKey
+	{
+	public:
+		enum Type { None, PGP, X509 };
+		SecureMessageKey();
+		SecureMessageKey(const SecureMessageKey &from);
+		~SecureMessageKey();
+		SecureMessageKey & operator=(const SecureMessageKey &from);
+
+		Type type() const;
+
+		// pgp
+		QString pgpPublicKey() const;
+		QString pgpSecretKey() const;
+		void setPGPPublicKey(const QString &id, const QString &name);
+		void setPGPSecretKey(const QString &id);
+
+		// x509
+		Certificate x509Certificate() const;
+		PrivateKey x509PrivateKey() const;
+		void setX509Certificate(const Certificate &c);
+		void setX509PrivateKey(const PrivateKey &k);
+
+		// generic
+		bool havePrivate() const;
+		QString id() const;
+		QString name() const;
+
+	private:
+		class Private;
+		Private *d;
+	};
+	typedef QValueList<SecureMessageKey> SecureMessageKeyList;
+
 	class SecureMessage : public QObject
 	{
 		Q_OBJECT
 	public:
-		// encrypt, decrypt, sign, verify
+		enum Mode { EncryptThenSign, SignThenEncrypt };
+		enum Error { ErrBadPassphrase, ErrUnknown };
+		enum VerifyResult { VerifyGood, VerifyBad, VerifyNoKey, VerifyError };
+		SecureMessage(SecureMessageSystem *system);
+		~SecureMessage();
+
+		bool canEncryptMultiple() const; // can smime do multiple?
+		void encrypt(const QSecureArray &in, const SecureMessageKey &key);
+		void encrypt(const QSecureArray &in, const SecureMessageKeyList &keys);
+		void encryptAndSign(const QSecureArray &in, const SecureMessageKey &key, const SecureMessageKey &signer, Mode m = EncryptThenSign);
+		void encryptAndSign(const QSecureArray &in, const SecureMessageKeyList &keys, const SecureMessageKey &signer, Mode m = EncryptThenSign);
+		void decrypt(const QString &in);
+		void sign(const QSecureArray &in, const SecureMessageKey &signer);
+		void verify(const QSecureArray &in, const QString &sig);
+		bool waitForFinished();
+
+		bool success() const;
+		Error errorCode() const;
+		QString encrypted() const;
+		QSecureArray decrypted() const;
+		QString signature() const;
+		SecureMessageKey key() const;
+		QDateTime timestamp() const;
+		VerifyResult verifyResult();
+
+	signals:
+		void finished();
+
+	public:
+		class Private;
+	private:
+		friend class Private;
+		Private *d;
 	};
 
 	class SecureMessageSystem : public QObject
 	{
 		Q_OBJECT
 	public:
-		// setup, passphrase control
+		SecureMessageSystem(QObject *parent = 0, const char *name = 0);
+		~SecureMessageSystem();
 	};
 
-	class OpenPGP : public SecureMessageSystem
+	class OpenPGP : public SecureMessageSystem, public Algorithm
 	{
 		Q_OBJECT
 	public:
+		OpenPGP(QObject *parent = 0, const char *name = 0, const QString &provider = QString());
+		~OpenPGP();
+
+		void setAllowAgent(bool);
+		void submitPassphrase(const QSecureArray &passphrase);
+
+		SecureMessageKeyList secretKeys() const;
+		SecureMessageKeyList publicKeys() const;
+
+	signals:
+		void keysUpdated();
+		void needPassphrase();
 	};
 
-	class SMIME : public SecureMessageSystem
+	class SMIME : public SecureMessageSystem, public Algorithm
 	{
 		Q_OBJECT
 	public:
+		SMIME(QObject *parent = 0, const char *name = 0, const QString &provider = QString());
+		~SMIME();
+
+		void setStore(const Store &store);
 	};
 }
 
--- src/qca_securemessage.cpp
+++ src/qca_securemessage.cpp
@@ -21,22 +21,260 @@
 
 #include "qca_securemessage.h"
 
+#include <qdatetime.h>
+#include "qca_publickey.h"
+#include "qca_cert.h"
+
 namespace QCA {
+
+//----------------------------------------------------------------------------
+// SecureMessageKey
+//----------------------------------------------------------------------------
+SecureMessageKey::SecureMessageKey()
+{
+}
+
+SecureMessageKey::SecureMessageKey(const SecureMessageKey &from)
+{
+	Q_UNUSED(from);
+}
+
+SecureMessageKey::~SecureMessageKey()
+{
+}
+
+SecureMessageKey & SecureMessageKey::operator=(const SecureMessageKey &from)
+{
+	Q_UNUSED(from);
+	return *this;
+}
+
+SecureMessageKey::Type SecureMessageKey::type() const
+{
+	return None;
+}
+
+QString SecureMessageKey::pgpPublicKey() const
+{
+	return QString();
+}
+
+QString SecureMessageKey::pgpSecretKey() const
+{
+	return QString();
+}
+
+void SecureMessageKey::setPGPPublicKey(const QString &id, const QString &name)
+{
+	Q_UNUSED(id);
+	Q_UNUSED(name);
+}
+
+void SecureMessageKey::setPGPSecretKey(const QString &id)
+{
+	Q_UNUSED(id);
+}
+
+Certificate SecureMessageKey::x509Certificate() const
+{
+	return Certificate();
+}
+
+PrivateKey SecureMessageKey::x509PrivateKey() const
+{
+	return PrivateKey();
+}
+
+void SecureMessageKey::setX509Certificate(const Certificate &c)
+{
+	Q_UNUSED(c);
+}
+
+void SecureMessageKey::setX509PrivateKey(const PrivateKey &k)
+{
+	Q_UNUSED(k);
+}
+
+bool SecureMessageKey::havePrivate() const
+{
+	return false;
+}
+
+QString SecureMessageKey::id() const
+{
+	return QString();
+}
+
+QString SecureMessageKey::name() const
+{
+	return QString();
+}
 
 //----------------------------------------------------------------------------
 // SecureMessage
 //----------------------------------------------------------------------------
+SecureMessage::SecureMessage(SecureMessageSystem *system)
+{
+	Q_UNUSED(system);
+}
+
+SecureMessage::~SecureMessage()
+{
+}
+
+bool SecureMessage::canEncryptMultiple() const
+{
+	return false;
+}
+
+void SecureMessage::encrypt(const QSecureArray &in, const SecureMessageKey &key)
+{
+	Q_UNUSED(in);
+	Q_UNUSED(key);
+}
+
+void SecureMessage::encrypt(const QSecureArray &in, const SecureMessageKeyList &keys)
+{
+	Q_UNUSED(in);
+	Q_UNUSED(keys);
+}
+
+void SecureMessage::encryptAndSign(const QSecureArray &in, const SecureMessageKey &key, const SecureMessageKey &signer, Mode m)
+{
+	Q_UNUSED(in);
+	Q_UNUSED(key);
+	Q_UNUSED(signer);
+	Q_UNUSED(m);
+}
+
+void SecureMessage::encryptAndSign(const QSecureArray &in, const SecureMessageKeyList &keys, const SecureMessageKey &signer, Mode m)
+{
+	Q_UNUSED(in);
+	Q_UNUSED(keys);
+	Q_UNUSED(signer);
+	Q_UNUSED(m);
+}
+
+void SecureMessage::decrypt(const QString &in)
+{
+	Q_UNUSED(in);
+}
+
+void SecureMessage::sign(const QSecureArray &in, const SecureMessageKey &signer)
+{
+	Q_UNUSED(in);
+	Q_UNUSED(signer);
+}
+
+void SecureMessage::verify(const QSecureArray &in, const QString &sig)
+{
+	Q_UNUSED(in);
+	Q_UNUSED(sig);
+}
+
+bool SecureMessage::waitForFinished()
+{
+	return false;
+}
+
+bool SecureMessage::success() const
+{
+	return false;
+}
+
+SecureMessage::Error SecureMessage::errorCode() const
+{
+	return ErrUnknown;
+}
+
+QString SecureMessage::encrypted() const
+{
+	return QString();
+}
+
+QSecureArray SecureMessage::decrypted() const
+{
+	return QSecureArray();
+}
+
+QString SecureMessage::signature() const
+{
+	return QString();
+}
+
+SecureMessageKey SecureMessage::key() const
+{
+	return SecureMessageKey();
+}
+
+QDateTime SecureMessage::timestamp() const
+{
+	return QDateTime();
+}
+
+SecureMessage::VerifyResult SecureMessage::verifyResult()
+{
+	return VerifyError;
+}
 
 //----------------------------------------------------------------------------
 // SecureMessageSystem
 //----------------------------------------------------------------------------
+SecureMessageSystem::SecureMessageSystem(QObject *parent, const char *name)
+:QObject(parent, name)
+{
+}
+
+SecureMessageSystem::~SecureMessageSystem()
+{
+}
 
 //----------------------------------------------------------------------------
 // OpenPGP
 //----------------------------------------------------------------------------
+OpenPGP::OpenPGP(QObject *parent, const char *name, const QString &provider)
+:SecureMessageSystem(parent, name), Algorithm("openpgp", provider)
+{
+}
+
+OpenPGP::~OpenPGP()
+{
+}
+
+void OpenPGP::setAllowAgent(bool)
+{
+}
+
+void OpenPGP::submitPassphrase(const QSecureArray &passphrase)
+{
+	Q_UNUSED(passphrase);
+}
+
+SecureMessageKeyList OpenPGP::secretKeys() const
+{
+	return SecureMessageKeyList();
+}
+
+SecureMessageKeyList OpenPGP::publicKeys() const
+{
+	return SecureMessageKeyList();
+}
 
 //----------------------------------------------------------------------------
 // SMIME
 //----------------------------------------------------------------------------
+SMIME::SMIME(QObject *parent, const char *name, const QString &provider)
+:SecureMessageSystem(parent, name), Algorithm("smime", provider)
+{
+}
+
+SMIME::~SMIME()
+{
+}
+
+void SMIME::setStore(const Store &store)
+{
+	Q_UNUSED(store);
+}
 
 }
