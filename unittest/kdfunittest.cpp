ff56c7ab4394ed9d79eb5290ac5cdd3911d9e7df
Brad Hards <bradh@frogmouth.net>
2005-03-29 11:36:26 +0000

    Add some additional unit tests - this time from Botan's tests.
    Also does a trivial whitespace change.
    
    svn path=/trunk/kdesupport/qca/; revision=401574

--- unittest/kdfunittest.cpp
+++ unittest/kdfunittest.cpp
@@ -61,6 +61,21 @@ static struct kdfTestValues pbkdf1TestValues[] = {
     { 0, 0, 0, 0, 0 }
 };
 
+// These are from Botan's test suite
+static struct kdfTestValues pbkdf2TestValues[] = {
+    { "6a79756571677872736367676c707864796b6366", "df6d9d72872404bf73e708cf3b7d", "9b56e55328a4c97a250738f8dba1b992e8a1b508", 14, 10000 },
+    { "61717271737a6e7a76767a67746b73616d6d676f", "fa13f40af1ade2a30f2fffd66fc8a659ef95e6388c1682fc0fe4d15a70109517a32942e39c371440", "57487813cdd2220dfc485d932a2979ee8769ea8b", 40, 101 },
+    { "6c7465786d666579796c6d6c62727379696b6177", "027afadd48f4be8dcc4f", "ed1f39a0a7f3889aaf7e60743b3bc1cc2c738e60", 10, 1000 },
+    { "6378676e7972636772766c6c796c6f6c736a706f", "7c0d009fc91b48cb6d19bafbfccff3e2ccabfe725eaa234e56bde1d551c132f2", "94ac88200743fb0f6ac51be62166cbef08d94c15", 32, 1 },
+    { "7871796668727865686965646c6865776e76626a", "4661301d3517ca4443a6a607b32b2a63f69996299df75db75f1e0b98dd0eb7d8", "24a1a50b17d63ee8394b69fc70887f4f94883d68", 32, 5 },
+    { "616e6461716b706a7761627663666e706e6a6b6c", "82fb44a521448d5aac94b5158ead1e4dcd7363081a747b9f7626752bda2d", "9316c80801623cc2734af74bec42cf4dbaa3f6d5", 30, 100 },
+    { "687361767679766f636c6f79757a746c736e6975", "f8ec2b0ac817896ac8189d787c6424ed24a6d881436687a4629802c0ecce", "612cc61df3cf2bdb36e10c4d8c9d73192bddee05", 30, 100 },
+    { "6561696d72627a70636f706275736171746b6d77", "c9a0b2622f13916036e29e7462e206e8ba5b50ce9212752eb8ea2a4aa7b40a4cc1bf", "45248f9d0cebcb86a18243e76c972a1f3b36772a", 34, 100 },
+    { "67777278707178756d7364736d626d6866686d666463766c63766e677a6b6967", "4c9db7ba24955225d5b845f65ef24ef1b0c6e86f2e39c8ddaa4b8abd26082d1f350381fadeaeb560dc447afc68a6b47e6ea1e7412f6cf7b2d82342fccd11d3b4", "a39b76c6eec8374a11493ad08c246a3e40dfae5064f4ee3489c273646178", 64, 1000 },
+    { 0, 0, 0, 0, 0 }
+};
+
+
 KDFUnitTest::KDFUnitTest()
     : Tester()
 {
@@ -80,7 +95,7 @@ void KDFUnitTest::pbkdf1Tests()
     if(!QCA::isSupported("pbkdf1(sha1)"))
 	SKIP("PBKDF version 1 with SHA1 not supported");
     else {
-      for (int n = 0; (0 != pbkdf1TestValues[n].secret); n++) {
+	for (int n = 0; (0 != pbkdf1TestValues[n].secret); n++) {
 	    QSecureArray password = QCA::hexToArray( pbkdf1TestValues[n].secret );
 	    QCA::InitializationVector salt( QCA::hexToArray( pbkdf1TestValues[n].salt) );
 	    QCA::SymmetricKey key = QCA::PBKDF1().makeKey( password,
@@ -193,6 +208,15 @@ void KDFUnitTest::pbkdf2Tests()
 		   QString( "6b9cf26d45455a43a5b8bb276a403b39e7fe37a0c41e02c281ff3069e1e94f52" ) );
 	}
 
+	for (int n = 0; (0 != pbkdf2TestValues[n].secret); n++) {
+	    QSecureArray password = QCA::hexToArray( pbkdf2TestValues[n].secret );
+	    QCA::InitializationVector salt( QCA::hexToArray( pbkdf2TestValues[n].salt) );
+	    QCA::SymmetricKey key = QCA::PBKDF2().makeKey( password,
+							   salt,
+							   pbkdf2TestValues[n].outputLength,
+							   pbkdf2TestValues[n].iterationCount);
+	    CHECK( QCA::arrayToHex( key ), QString( pbkdf2TestValues[n].output ) );
+	}
     }
 
 }
