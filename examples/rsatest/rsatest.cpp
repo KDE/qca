1049dc5be1fba638b82aec7b15872c17ffd1a385
Brad Hards <bradh@frogmouth.net>
2005-04-04 11:43:11 +0000

    Update the RSA example to the new API.
    svn path=/trunk/kdesupport/qca/; revision=403095

--- examples/rsatest/rsatest.cpp
+++ examples/rsatest/rsatest.cpp
@@ -1,5 +1,6 @@
 /*
  Copyright (C) 2003 Justin Karneges
+ Copyright (C) 2005 Brad Hards
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
@@ -19,90 +20,97 @@
  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-// TODO: this code needs to be updated for QCA2
-#include<qfile.h>
-#include<qfileinfo.h>
-#include"qca.h"
-#include<stdio.h>
+#include <QtCrypto>
 
-//#define USE_FILE
-
-QCA::RSAKey readKeyFile(const QString &name)
-{
-	QCA::RSAKey k;
-	QFile f(name);
-	if(!f.open(IO_ReadOnly)) {
-		printf("Unable to open %s\n", name.latin1());
-		return k;
-	}
-	QByteArray der = f.readAll();
-	f.close();
-	printf("Read %s [%d bytes]\n", name.latin1(), der.size());
-
-	if(!k.fromDER(der)) {
-		printf("%s: Error importing DER format.\n", name.latin1());
-		return k;
-	}
-	char *yes = "yes";
-	char *no = "no";
-	printf("Successfully imported %s (enc=%s, dec=%s)\n",
-		name.latin1(),
-		k.havePublic() ? yes : no,
-		k.havePrivate() ? yes : no);
-
-	printf("Converting to DER: %d bytes\n", k.toDER().size());
-	printf("Converting to PEM:\n%s\n", k.toPEM().latin1());
-	return k;
-}
+#include <iostream>
 
 int main(int argc, char **argv)
 {
-	QCA::init();
-	QCString cs = (argc >= 2) ? argv[1] : "hello";
+    // the Initializer object sets things up, and 
+    // also does cleanup when it goes out of scope
+    QCA::Initializer init;
+    
+    QCoreApplication app(argc, argv);
+    
+    // we use the first argument if provided, or
+    // use "hello" if no arguments
+    QSecureArray arg = (argc >= 2) ? argv[1] : "hello";
 
-	if(!QCA::isSupported(QCA::CAP_RSA))
-		printf("RSA not supported!\n");
-	else {
-#ifdef USE_FILE
-		QCA::RSAKey pubkey = readKeyFile("keypublic.der");
-		if(pubkey.isNull())
-			return 1;
-		QCA::RSAKey seckey = readKeyFile("keyprivate.der");
-		if(seckey.isNull())
-			return 1;
-#else
-		QCA::RSAKey seckey = QCA::RSA::generateKey(1024);
-		if(seckey.isNull())
-			return 1;
-		QCA::RSAKey pubkey = seckey;
-#endif
-		// encrypt some data
-		QByteArray a(cs.length());
-		memcpy(a.data(), cs.data(), a.size());
+    if(!QCA::isSupported("pkey") ||
+       !QCA::PKey::supportedTypes().contains(QCA::PKey::RSA) ||
+       !QCA::PKey::supportedIOTypes().contains(QCA::PKey::RSA))
+	printf("RSA not supported!\n");
+    else {
+	QCA::PrivateKey seckey = QCA::KeyGenerator().createRSA(1024);
+	if(seckey.isNull()) {
+	    std::cout << "Failed to make private RSA key" << std::endl;
+	    return 1;
+	}
+	QCA::PublicKey pubkey = seckey.toPublicKey();
 
-		QCA::RSA op;
-		op.setKey(pubkey);
-		QByteArray result;
-		if(!op.encrypt(a, &result)) {
-			printf("Error encrypting.\n");
-			return 1;
-		}
-		QString rstr = QCA::arrayToHex(result);
-		printf(">rsa(\"%s\") = [%s]\n", cs.data(), rstr.latin1());
-
-		// now decrypt it
-		op.setKey(seckey);
-		QByteArray dec;
-		if(!op.decrypt(result, &dec)) {
-			printf("Error decrypting.\n");
-			return 1;
-		}
-		QCString dstr;
-		dstr.resize(dec.size()+1);
-		memcpy(dstr.data(), dec.data(), dec.size());
-		printf("<rsa(\"%s\") = [%s]\n", rstr.latin1(), dstr.data());
+	// check if the key can encrypt
+	if(!pubkey.canEncrypt())
+	{
+	    std::cout << "Error: this kind of key cannot encrypt" << std::endl;
+	    return 1;
 	}
 
-	return 0;
+	// encrypt some data
+	// you must also choose the algorithm to be used
+	QSecureArray result = pubkey.encrypt(arg, QCA::EME_PKCS1_OAEP);
+	if(result.isEmpty()) {
+	    std::cout << "Error encrypting" << std::endl;
+	    return 1;
+	}
+
+	// output the encrypted data
+	QString rstr = QCA::arrayToHex(result);
+	std::cout << "\"" << arg.data() << "\" encrypted with RSA is \"";
+	std::cout << qPrintable(rstr) << "\"" << std::endl;
+
+	// save the private key - in a real example, make sure this goes
+	// somewhere secure! 
+	// you can use the same technique with the public key too.
+	QSecureArray passPhrase = "pass phrase";
+	seckey.toPEMFile("keyprivate.pem", passPhrase);
+
+	// Read that key back in, checking if the read succeeded
+	QCA::ConvertResult conversionResult;
+	QCA::PrivateKey privateKey = QCA::PrivateKey::fromPEMFile("keyprivate.pem", passPhrase, &conversionResult);
+	if (! QCA::ConvertGood == conversionResult) {
+	    std::cout << "Private key read failed" << std::endl;
+	}
+
+	// now decrypt that encrypted data using the private key that
+	// we read in. The algorithm is the same.
+	QSecureArray decrypt;
+	if(0 == privateKey.decrypt(result, &decrypt, QCA::EME_PKCS1_OAEP)) {
+	    printf("Error decrypting.\n");
+	    return 1;
+	}
+
+	// output the resulting decrypted string
+	std::cout << "\"" << qPrintable(rstr) << "\" decrypted with RSA is \"";
+	std::cout << decrypt.data() << "\"" << std::endl;
+
+
+
+
+
+
+
+
+#if 0
+	QCA::RSAKey pubkey = readKeyFile("keypublic.der");
+	if(pubkey.isNull())
+	    return 1;
+	QCA::RSAKey seckey = readKeyFile("keyprivate.der");
+	if(seckey.isNull())
+	    return 1;
+#endif
+
+    }
+
+    return 0;
 }
 
