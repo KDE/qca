ec8a3946d684e747051633eefaee658278d37fe6
Brad Hards <bradh@frogmouth.net>
2006-04-03 10:22:47 +0000

    Update to remove the signature() calls.
    svn path=/trunk/kdesupport/qca/; revision=525921

--- unittest/certunittest/certunittest.cpp
+++ unittest/certunittest/certunittest.cpp
@@ -121,17 +121,6 @@ void CertUnitTest::qualitysslcatest()
 	    QCOMPARE( ca1.isCA(), true );
 	    QCOMPARE( ca1.isSelfSigned(), false );
 
-	    QSecureArray expectedSig = 
-		QCA::hexToArray("3a1e246fdadb366cfe3a7339ddcd6a1c69a1"
-				"001f6fd4aed51fd48829a521c257f6557b30"
-				"e7d264d60ec4aa703c7674e22aaec7466732"
-				"92c94bf1a7d7e056bcf672109d7fb075d69d"
-				"57b57185aac43aa74bb8ec0fe6d2f8ffb5cd"
-				"d44525acea06a78ab5cd3222e421921befce"
-				"34ae30441aee9b7ff09411868a871901283a"
-				"cfb3");
-	    QCOMPARE( ca1.signature(), expectedSig );
-    
 	    QCOMPARE( ca1.signatureAlgorithm(), QCA::EMSA3_SHA1 );
 
 	    QCOMPARE( ca1.serialNumber(), QBigInteger("33555098") );
@@ -187,24 +176,6 @@ void CertUnitTest::checkClientCerts()
 	    
 	    QCOMPARE( client1.commonName(), QString("Insecure User Test Cert") );
 
-	    QSecureArray expectedSig = 
-		QCA::hexToArray("791044711ff1a78a4b1f755b4264db6441a3"
-				"544043c4d01aa6c7480eb281d5e700dc9321"
-				"6438f078a738306e4cf8543985c093f244a2"
-				"fba6cbd79049472c8c078ef6ecd4520b58bf"
-				"d959199941a1864dc047d523b2fac20b4d03"
-				"80fb877503eb23197df822b6eb2a404e2cdb"
-				"e556a9ce6a0173607f75679587e2896c4920"
-				"4ee75f163f7c0ea9fc9225276ea6c2dce30f"
-				"6b5ba27b3f287abf21e8e0323a29e6c746ad"
-				"8d3a92e5238b23edbea75969307321a1d9f8"
-				"8e099df0f9ac290d23332034650d6da33417"
-				"3b0f55a5161e82c9bc2ccab47a675fc9bc69"
-				"c066eb088224ec15e30480eb8586e76f718a"
-				"6e5ca4cede1ba8e783b49b9383204e4b72dd"
-				"c6a81fce");
-	    QCOMPARE( client1.signature(), expectedSig );
-	    
 	    QCOMPARE( client1.notValidBefore().toString(), QDateTime( QDate( 2001, 8, 17 ), QTime( 8, 32, 38 ), Qt::UTC ).toString() );
 	    QCOMPARE( client1.notValidAfter().toString(), QDateTime( QDate( 2006, 8, 16 ), QTime( 8, 32, 38 ), Qt::UTC ).toString() );
 	    
@@ -432,24 +403,6 @@ void CertUnitTest::checkServerCerts()
 
 	    QCOMPARE( server1.commonName(), QString("Insecure Server Cert") );
 	    
-	    QSecureArray expectedSig = 
-		QCA::hexToArray("43e7896443b7ffb4d579dcfef1566de3b871"
-				"d9bdecf67c1b017ffa6f41192fa8f812826e"
-				"5305f5ff4f265efd471f43d2cf6616476ece"
-				"eeb9d9a2b9fff32af6ecc301b20b990c64de"
-				"65dd9bd66a0e0d41912b68059c6d94e63240"
-				"25babc3b8b80df8436dfcacef43a918a0655"
-				"a6730fbba13ef0a12197ddc952d3613f1056"
-				"c0fa0ee717cbf1def2d309b7538f9643fb12"
-				"c2578eb60077cb6727e07709e285d968e7e5"
-				"75a94254ea812ccc3a2936c6322dd0153d89"
-				"d26aac86ec4b412d3ad03141c37fcbd3a81d"
-				"7a5c1a6bdfe539441922f06d762f07d9e6b4"
-				"8ce65f40e740d24fd429dfe3a7d49eaf9001"
-				"f876550f69a1c13f1852133f64f47fb81b3c"
-				"cc6a8812" );
-	    QCOMPARE( server1.signature(), expectedSig );
-
 	    QCOMPARE( server1.notValidBefore().toString(), QDateTime( QDate( 2001, 8, 17 ), QTime( 8, 46, 24 ), Qt::UTC ).toString() );
 	    QCOMPARE( server1.notValidAfter().toString(), QDateTime( QDate( 2006, 8, 16 ), QTime( 8, 46, 24 ), Qt::UTC ).toString() );
 	    
@@ -573,23 +526,6 @@ void CertUnitTest::crl()
 	    QCOMPARE( crl1.nextUpdate(), QDateTime(QDate(2006, 8, 16), QTime(11, 12, 03)) );
 
 	    QCOMPARE( crl1.signatureAlgorithm(), QCA::EMSA3_MD5 );
-	    QSecureArray expectedSig = 
-		QCA::hexToArray("78ee531508a94fa010202f06b2efa78d46ad"
-				"7be3d8edffb83f3ae874ea2b0d33aebf0a0c"
-				"d52f6c4a482decd855f8c4a837db49e2588e"
-				"83be6e07dcce88ed1beea02bd9055a6ed4c0"
-				"3ccf6368f3d83650f5a54b1500beebe8d863"
-				"86460a3ef2773ff3883c6e22d4c53485f15b"
-				"384f51e7828d6aadf78f338a2ed6ebbb2e14"
-				"6747cb6e215a104fa5b7452944b1044144cd"
-				"f2f9590e3c07f41811b297a5865a2ca65c7a"
-				"acaa2b3cd166d7e74b6bf39f4296da9bbb6c"
-				"9a1405fef8810104307b268562d5b96e9a06"
-				"aba9f5d89285169fd77717cab248d8ec7e13"
-				"95eb5ddf5e999f5c5677dee4c5b0b4a88a13"
-				"fc7faf82bacc76adfb866a9ee338fbfb8fbb"
-				"23976635");
-	    QCOMPARE( crl1.signature(), expectedSig );
 
 	    QCOMPARE( crl1.issuerKeyId(), QByteArray("") );
 	    QCOMPARE( crl1, QCA::CRL(crl1) );
@@ -646,16 +582,6 @@ void CertUnitTest::crl2()
 	    QCOMPARE( crl1.nextUpdate(), QDateTime(QDate(2011, 4, 19), QTime(14, 57, 20)) );
 
 	    QCOMPARE( crl1.signatureAlgorithm(), QCA::EMSA3_SHA1 );
-	    QSecureArray expectedSig = 
-		QCA::hexToArray("93c2ec0b71072d9dd7a2b3f0ed084d6e0690"
-				"667206a9c23073f11872bfa7511395c4313f"
-				"1d7941ededabd096111e32474cc4f7e20865"
-				"6f7355c1590956f2607927182e9440dd7eb1"
-				"92bfb857e54cc53897752aa117a2250dec0e"
-				"b795408d2cdfb9fa10ffbe9e4af2374f25cb"
-				"1bc86defe409b903361bc1d9f94f005e8085"
-				"92cd");
-	    QCOMPARE( crl1.signature(), expectedSig );
 
 	    QCOMPARE( QCA::arrayToHex( crl1.issuerKeyId() ), QString("b72ea682cbc2c8bca87b2744d73533df9a1594c7") );
 	    QCOMPARE( crl1.number(), 1 );
@@ -736,16 +662,6 @@ void CertUnitTest::csr()
 	    QCOMPARE( rsaPkey.e(), QBigInteger(65537) );
 	    QCOMPARE( rsaPkey.n(), QBigInteger("104853561647822232509211983664549572246855698961210758585652966258891659217901732470712446421431206166165309547771124747713609923038218156616083520796442797276676074122658684367500665423564881889504308700315044585826841844654287577169905826705891670004942854611681809539126326134927995969418712881512819058439") );
 
-	    QSecureArray expectedSig = 
-		QCA::hexToArray("8c53f3db5efb261c8563b21d09bacec49bc6"
-				"46d34d20a73130184069103c2ba0e34a92fe"
-				"40b0fd0e911c45a40548b61d8df4318c4de8"
-				"1f300bf860b07bae6ad01c5f15ecbf821d23"
-				"7e3c45a9696efc9e83beda5a13a53ed12a62"
-				"21a6ade3a8be12016623d34523cc35951273"
-				"4f416384aa518b47d6c08b74c0fd12305ba9"
-				"e3fa");
-	    QCOMPARE( csr1.signature(), expectedSig );
 	    QCOMPARE( csr1.signatureAlgorithm(), QCA::EMSA3_MD5 );
 	}
     }
@@ -784,16 +700,6 @@ void CertUnitTest::csr2()
 	    QCOMPARE( rsaPkey.e(), QBigInteger(65537) );
 	    QCOMPARE( rsaPkey.n(), QBigInteger("151872780463004414908584891835397365176526767139347372444365914360701714510188717169754430290680734981291754624394094502297070722505032645306680495915914243593438796635264236530526146243919417744996366836534380790370421346490191416041004278161146551997010463199760480957900518811859984176646089981367745961681" ) );
 
-	    QSecureArray expectedSig = 
-		QCA::hexToArray("5239cf0fcbbd1a87ad7184e235fa7e447edf"
-				"76841c081c9e2fad482b6d3bb7578e253281"
-				"67a760cc64cb2f83acd9c7665779cbea8a90"
-				"8ceb46f627fff7ad3e73e368639ba5369937"
-				"57739126b35a7173c511be5a40d6a95b6db5"
-				"11d75a78f0845d7d9672fa9cf920abe36d1f"
-				"6ca8b6f2eeb12632d4741f1b8939c24f717a"
-				"4c59");
-	    QCOMPARE( csr1.signature(), expectedSig );
 	    QCOMPARE( csr1.signatureAlgorithm(), QCA::EMSA3_MD5 );
 
 	    // convert to DER
