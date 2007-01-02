9cef86062cefaa4f25758a90ef4d92ca501497d0
Brad Hards <bradh@frogmouth.net>
2007-01-02 05:41:00 +0000

    Pass by reference, where possible.
    Also update FSF address.
    
    svn path=/trunk/kdesupport/qca/; revision=618715

--- plugins/qca-gcrypt/qca-gcrypt.cpp
+++ plugins/qca-gcrypt/qca-gcrypt.cpp
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  *
  */
 #include <QtCore>
@@ -28,7 +28,7 @@ namespace gcryptQCAPlugin {
 
 #include "pkcs5.c"
 
-void check_error( QString label, gcry_error_t err )
+void check_error( const QString &label, gcry_error_t err )
 {
     // we ignore the case where it is not an error, and
     // we also don't flag weak keys.
