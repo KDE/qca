8b495aaf324efc2a5644d5a0ce8cd54206d41a03
Brad Hards <bradh@frogmouth.net>
2007-02-03 07:13:38 +0000

    Make sure that the QCoreApplication continues to exist (not actually required in this case, but good practice in general).
    Also some gratuitous whitespace changes.
    
    svn path=/trunk/kdesupport/qca/; revision=629566

--- examples/hextest/hextest.cpp
+++ examples/hextest/hextest.cpp
@@ -7,10 +7,10 @@
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
@@ -26,9 +26,9 @@
 
 int main(int argc, char **argv)
 {
-	QCoreApplication(argc, argv);
+	QCoreApplication app(argc, argv);
 
-	// the Initializer object sets things up, and 
+	// the Initializer object sets things up, and
 	// also does cleanup when it goes out of scope
 	QCA::Initializer init;
 
