699aa6857e504daa3ec51f99431834cff317bccd
Brad Hards <bradh@frogmouth.net>
2007-04-03 11:07:19 +0000

    trivial whitespace cleanups + add email address.
    svn path=/trunk/kdesupport/qca/; revision=649896

--- examples/rsatest/rsatest.cpp
+++ examples/rsatest/rsatest.cpp
@@ -1,6 +1,6 @@
 /*
  Copyright (C) 2003 Justin Karneges
- Copyright (C) 2005 Brad Hards
+ Copyright (C) 2005 Brad Hards <bradh@frogmouth.net>
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
@@ -8,10 +8,10 @@
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
@@ -29,9 +29,9 @@ int main(int argc, char **argv)
     // The Initializer object sets things up, and also
     // does cleanup when it goes out of scope
     QCA::Initializer init;
-    
+
     QCoreApplication app(argc, argv);
-    
+
     // we use the first argument if provided, or
     // use "hello" if no arguments
     QSecureArray arg = (argc >= 2) ? argv[1] : "hello";
@@ -144,7 +144,7 @@ int main(int argc, char **argv)
 	} else {
 	    std::cout << "Signature could not be verified" << std::endl;
 	}
-	    
+
     }
 
     return 0;
