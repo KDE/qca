c26eac034982d318318dd0e7ac1887650451153c
Brad Hards <bradh@frogmouth.net>
2007-04-03 11:27:47 +0000

    EBN fix: slots: -> Q_SLOTS.
    Not really a problem in this case, but no loss.
    
    svn path=/trunk/kdesupport/qca/; revision=649910

--- unittest/clientplugin/clientplugin.h
+++ unittest/clientplugin/clientplugin.h
@@ -32,7 +32,7 @@ class ClientPlugin : public QObject
 {
     Q_OBJECT
 
-private slots:
+private Q_SLOTS:
     void initTestCase();
     void cleanupTestCase();
     void testInsertPlugin();
--- unittest/filewatchunittest/filewatchunittest.h
+++ unittest/filewatchunittest/filewatchunittest.h
@@ -32,7 +32,7 @@ class FileWatchUnitTest : public QObject
 {
     Q_OBJECT
 
-private slots:
+private Q_SLOTS:
     void initTestCase();
     void cleanupTestCase();
     void filewatchTest();
