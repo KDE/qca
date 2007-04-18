d504b7429ae230971849f6f4cc097100f963add5
Brad Hards <bradh@frogmouth.net>
2007-04-18 11:08:51 +0000

    krazy fixes.
    svn path=/trunk/kdesupport/qca/; revision=655437

--- examples/saslservtest/saslservtest.cpp
+++ examples/saslservtest/saslservtest.cpp
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2003-2006 Justin Karneges, Michail Pishchagin
+ Copyright (C) 2003-2006 Justin Karneges <justin@affinix.com>, Michail Pishchagin
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
