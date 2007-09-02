8b904d7e81cd8d8f14cf9e55dae22c80f2d07333
Brad Hards <bradh@frogmouth.net>
2007-09-02 18:51:21 +0000

    Add unit test for Whirlpool hash.
    svn path=/trunk/kdesupport/qca/; revision=707709

--- unittest/hashunittest/hashunittest.cpp
+++ unittest/hashunittest/hashunittest.cpp
@@ -62,6 +62,9 @@ private slots:
     void rmd160test_data();
     void rmd160test();
     void rmd160longtest();
+    void whirlpooltest_data();
+    void whirlpooltest();
+    void whirlpoollongtest();
 private:
     QCA::Initializer* m_init;
 };
@@ -793,6 +796,86 @@ void HashUnitTest::rmd160longtest()
 		rmdHash.update(fillerArray);
 	    QCOMPARE( QString(QCA::arrayToHex(rmdHash.final().toByteArray())),
 		     QString("9b752e45573d4b39f4dbd3323cab82bf63326bfb") );
+
+	}
+    }
+}
+
+
+// These are from the documentation pack at http://paginas.terra.com.br/informatica/paulobarreto/WhirlpoolPage.html
+void HashUnitTest::whirlpooltest_data()
+{
+    QTest::addColumn<QByteArray>("input");
+    QTest::addColumn<QString>("expectedHash");
+
+    QTest::newRow("whirlpool()") << QByteArray("") << QString("19fa61d75522a4669b44e39c1d2e1726c530232130d407f89afee0964997f7a73e83be698b288febcf88e3e03c4f0757ea8964e59b63d93708b138cc42a66eb3");
+    QTest::newRow("whirlpool(a)") << QByteArray("a") << QString("8aca2602792aec6f11a67206531fb7d7f0dff59413145e6973c45001d0087b42d11bc645413aeff63a42391a39145a591a92200d560195e53b478584fdae231a");
+    QTest::newRow("whirlpool(abc)") << QByteArray("abc") << QString("4e2448a4c6f486bb16b6562c73b4020bf3043e3a731bce721ae1b303d97e6d4c7181eebdb6c57e277d0e34957114cbd6c797fc9d95d8b582d225292076d4eef5");
+    QTest::newRow("whirlpool(md)") << QByteArray("message digest") << QString("378c84a4126e2dc6e56dcc7458377aac838d00032230f53ce1f5700c0ffb4d3b8421557659ef55c106b4b52ac5a4aaa692ed920052838f3362e86dbd37a8903e");
+    QTest::newRow("whirlpool(a-k)") << QByteArray("abcdbcdecdefdefgefghfghighijhijk")
+			      << QString("2a987ea40f917061f5d6f0a0e4644f488a7a5a52deee656207c562f988e95c6916bdc8031bc5be1b7b947639fe050b56939baaa0adff9ae6745b7b181c3be3fd");
+    QTest::newRow("whirlpool(a-z)") << QByteArray("abcdefghijklmnopqrstuvwxyz") << QString("f1d754662636ffe92c82ebb9212a484a8d38631ead4238f5442ee13b8054e41b08bf2a9251c30b6a0b8aae86177ab4a6f68f673e7207865d5d9819a3dba4eb3b");
+    QTest::newRow("whirlpool(A-9)") << QByteArray("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+			      << QString("dc37e008cf9ee69bf11f00ed9aba26901dd7c28cdec066cc6af42e40f82f3a1e08eba26629129d8fb7cb57211b9281a65517cc879d7b962142c65f5a7af01467");
+    QTest::newRow("whirlpool(1-0)") << QByteArray("12345678901234567890123456789012345678901234567890123456789012345678901234567890")
+				    << QString("466ef18babb0154d25b9d38a6414f5c08784372bccb204d6549c4afadb6014294d5bd8df2a6c44e538cd047b2681a51a2c60481e88c5a20b2c2a80cf3a9a083b");
+
+}
+
+
+void HashUnitTest::whirlpooltest()
+{
+    QStringList providersToTest;
+    providersToTest.append("qca-ossl");
+
+    QFETCH(QByteArray, input);
+    QFETCH(QString, expectedHash);
+
+    foreach(QString provider, providersToTest) {
+	if(!QCA::isSupported("whirlpool", provider))
+	    QWARN(QString("Whirlpool not supported for "+provider).toLocal8Bit());
+	else {
+	    QString hashResult = QCA::Hash("whirlpool", provider).hashToString(input);
+	    QCOMPARE( hashResult, expectedHash );
+	}
+    }
+}
+
+void HashUnitTest::whirlpoollongtest()
+{
+    QByteArray fillerString;
+    fillerString.fill('a', 1000);
+
+    QStringList providersToTest;
+    providersToTest.append("qca-ossl");
+
+    foreach(QString provider, providersToTest) {
+	if(!QCA::isSupported("whirlpool", provider))
+	    QWARN(QString("Whirlpool not supported for "+provider).toLocal8Bit());
+	else {
+	    QCA::Hash rmdHash("whirlpool", provider);
+
+	    // This is the "million times 'a' test"
+	    for (int i=0; i<1000; i++)
+	        rmdHash.update(fillerString);
+	    QCOMPARE( QString(QCA::arrayToHex(rmdHash.final().toByteArray())),
+		     QString("0c99005beb57eff50a7cf005560ddf5d29057fd86b20bfd62deca0f1ccea4af51fc15490eddc47af32bb2b66c34ff9ad8c6008ad677f77126953b226e4ed8b01") );
+
+	    rmdHash.clear();
+	    for (int i=0; i<1000; i++)
+		rmdHash.update(fillerString);
+	    QCOMPARE( QString(QCA::arrayToHex(rmdHash.final().toByteArray())),
+		     QString("0c99005beb57eff50a7cf005560ddf5d29057fd86b20bfd62deca0f1ccea4af51fc15490eddc47af32bb2b66c34ff9ad8c6008ad677f77126953b226e4ed8b01") );
+
+	    // This is the "8 rounds of 1234567890" test.
+	    // It also ensure that we can re-use hash objects correctly.
+	    static char bindata[] = { 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30 };
+	    QByteArray fillerArray( bindata, sizeof(bindata) ); // "1234567890"
+	    rmdHash.clear();
+	    for (int i=0; i<8; i++)
+		rmdHash.update(fillerArray);
+	    QCOMPARE( QString(QCA::arrayToHex(rmdHash.final().toByteArray())),
+		     QString("466ef18babb0154d25b9d38a6414f5c08784372bccb204d6549c4afadb6014294d5bd8df2a6c44e538cd047b2681a51a2c60481e88c5a20b2c2a80cf3a9a083b") );
 
 	}
     }
