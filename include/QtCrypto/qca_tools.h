36fc2a5705979497adaabd798b3782439fed4421
Justin Karneges <justin@affinix.com>
2008-06-05 02:01:46 +0000

    should be 'note' not 'Note'
    svn path=/trunk/kdesupport/qca/; revision=816994

--- include/QtCrypto/qca_tools.h
+++ include/QtCrypto/qca_tools.h
@@ -28,7 +28,7 @@
    These classes differ from those in qca_support.h, in that they have
    some cryptographic relationship, and require secure memory.
 
-   \Note You should not use this header directly from an
+   \note You should not use this header directly from an
    application. You should just use <tt> \#include \<QtCrypto>
    </tt> instead.
 */
