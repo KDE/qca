011a2f01644770ba8dd65ba0ba0a64639993bc15
Justin Karneges <justin@affinix.com>
2008-07-21 01:11:59 +0000

    oops, qputenv is not available in 4.2.  copy it from 4.4 for now.
    svn path=/trunk/kdesupport/qca/; revision=835695

--- unittest/pgpunittest/pgpunittest.cpp
+++ unittest/pgpunittest/pgpunittest.cpp
@@ -28,11 +28,25 @@
 
 #include <stdlib.h>
 
+// qt did not introduce qputenv until 4.4, so we'll keep a copy here for 4.2
+//   compat
+bool my_qputenv(const char *varName, const QByteArray& value)
+{
+#if defined(_MSC_VER) && _MSC_VER >= 1400
+    return _putenv_s(varName, value.constData()) == 0;
+#else
+    QByteArray buffer(varName);
+    buffer += "=";
+    buffer += value;
+    return putenv(qstrdup(buffer.constData())) == 0;
+#endif
+}
+
 static int qca_setenv(const char *name, const char *value, int overwrite)
 {
     if (!overwrite && !qgetenv(name).isNull()) return 0;
 
-    if(qputenv(name, QByteArray(value)))
+    if(my_qputenv(name, QByteArray(value)))
         return 0; // success
     else
         return 1; // error
