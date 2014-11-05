f214d9c2a6d5636e9b646218e102953ce708a524
Ivan Romanov <drizt@land.ru>
2014-11-05 21:17:15 +0600

    added frameworks support to crypto.prf

--- crypto.prf.cmake
+++ crypto.prf.cmake
@@ -2,12 +2,25 @@ QCA_INCDIR = @CRYPTO_PRF_RELATIVE_PATH@@QCA_INCLUDE_INSTALL_DIR@
 QCA_LIBDIR = @CRYPTO_PRF_RELATIVE_PATH@@QCA_LIBRARY_INSTALL_DIR@
 
 CONFIG *= qt
-INCLUDEPATH += $$QCA_INCDIR/QtCrypto
-LIBS += -L$$QCA_LIBDIR
 
-LINKAGE = -l@QCA_LIB_NAME@
-CONFIG(debug, debug|release) {
-	windows:LINKAGE = -l@QCA_LIB_NAME@d
-	mac:LINKAGE = -l@QCA_LIB_NAME@_debug
+LINKAGE =
+
+exists($$QCA_LIBDIR/qca.framework) {
+	QMAKE_CXXFLAGS += -F$$QCA_LIBDIR
+	LIBS *= -F$$QCA_LIBDIR
+	INCLUDEPATH += $$QCA_LIBDIR/qca.framework/Headers
+	LINKAGE = -framework qca
 }
+
+# else, link normally
+isEmpty(LINKAGE) {
+	INCLUDEPATH += $$QCA_INCDIR/QtCrypto
+	LIBS += -L$$QCA_LIBDIR
+	LINKAGE = -lqca
+	CONFIG(debug, debug|release) {
+		windows:LINKAGE = -lqcad
+		mac:LINKAGE = -lqca_debug
+	}
+}
+
 LIBS += $$LINKAGE
